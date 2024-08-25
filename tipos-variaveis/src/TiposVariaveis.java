0e389341d3840505c7ea8d4e9c28a2197a63a906
slimafilipe <limafilipe.coding@gmail.com>
2024-08-25 02:19:25 -0300

    Estudo Tipo de Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+        byte idade = 27;
+        short ano = 2024;
+        int cep = 48800000;
+        long cpf = 86535729532L;
+        float pi = 3.14F;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
